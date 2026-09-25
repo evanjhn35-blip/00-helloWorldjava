d168170833c03a4ba62c3e6b400241344b47a563
ejehannin <ejehannin@350028R-F115X12.lise.kreizenn.bzh>
2026-09-25 15:10:47 +0200

    VRAI EXERCICE 4

--- src/pkg00/helloworld/HelloWorld.java
+++ src/pkg00/helloworld/HelloWorld.java
@@ -16,36 +16,36 @@ public class HelloWorld {
      */
     //EXO 5
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.print("saisir un nombre a : "); //ecrire le nombre a
-        int a = sc.nextInt(); //integrer l'entier a
-        
-        System.out.print(" saisir un nombre b : "); //ecrire le nombre b
-        int b = sc.nextInt(); //integrer l'entier b
-        int z; //integrer l'entier z
-        int res; //integrer l'entier res qui sera le resultat du calcul
-        z=a+b; // faire le calcul a+b
-        res=z*2; // multiplier le tout par 2
-        
-        
-        System.out.println("Le resultat de (a+b)*2 est : " +res ); //le resultat
+        Scanner sc = new Scanner(System.in);       
+        //System.out.print("saisir un nombre a : "); //ecrire le nombre a
+        //int a = sc.nextInt(); //integrer l'entier a       
+        //System.out.print(" saisir un nombre b : "); //ecrire le nombre b
+        //int b = sc.nextInt(); //integrer l'entier b
+        //int z; //integrer l'entier z
+        //int res; //integrer l'entier res qui sera le resultat du calcul
+        //z=a+b; // faire le calcul a+b
+        //res=z*2; // multiplier le tout par 2
+        //System.out.println("Le resultat de (a+b)*2 est : " +res ); //le resultat
         
         
         
     
         
         //EXO4
-        //System.out.print("saisir un nombre x : ");
-        //int x = sc.nextInt();
-        //System.out.print(" saisir un nombre y : ");
-        //int y = sc.nextInt();
-        //System.out.println("sans permutation, x : " +x +"  y: " +y );
-        //int z =0;
-        //z=x;
-        //x=y;
-        //y=z;
-        //System.out.println("apres permutation, x : " +x +"  y: " +y );
+        System.out.print("saisir un nombre x : ");
+        int x = sc.nextInt();
+        
+        System.out.print(" saisir un nombre y : ");
+        int y = sc.nextInt();
+        
+        System.out.println("sans permutation, x : " +x +"  y: " +y );
+        int z =0;
+        
+        z=x;
+        x=y;
+        y=z;
+        
+        System.out.println("apres permutation, x : " +x +"  y: " +y );
         
        
         //hello World
