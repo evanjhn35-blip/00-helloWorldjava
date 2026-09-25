41c668ebffaf0b494b4cf30933aaa13a205cca45
ejehannin <ejehannin@350028R-F115X12.lise.kreizenn.bzh>
2026-09-25 15:04:44 +0200

    EXERCICE 4

--- src/pkg00/helloworld/HelloWorld.java
+++ src/pkg00/helloworld/HelloWorld.java
@@ -17,21 +17,34 @@ public class HelloWorld {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("saisir un nombre x : ");
-        int x = sc.nextInt();
+        System.out.print("saisir un nombre a : "); //ecrire le nombre a
+        int a = sc.nextInt(); //integrer l'entier a
         
-        System.out.print(" saisir un nombre y : ");
-        int y = sc.nextInt();
+        System.out.print(" saisir un nombre b : "); //ecrire le nombre b
+        int b = sc.nextInt(); //integrer l'entier b
+        int z; //integrer l'entier z
+        int res; //integrer l'entier res qui sera le resultat du calcul
+        z=a+b; // faire le calcul a+b
+        res=z*2; // multiplier le tout par 2
         
-        System.out.println("sans permutation, x : " +x +"  y: " +y );
         
-        int z =0;
+        System.out.println("Le resultat de (a+b)*2 est : " +res ); //le resultat
         
-        z=x;
-        x=y;
-        y=z;
         
-        System.out.println("apres permutation, x : " +x +"  y: " +y );
+        
+    
+        
+        //EXO4
+        //System.out.print("saisir un nombre x : ");
+        //int x = sc.nextInt();
+        //System.out.print(" saisir un nombre y : ");
+        //int y = sc.nextInt();
+        //System.out.println("sans permutation, x : " +x +"  y: " +y );
+        //int z =0;
+        //z=x;
+        //x=y;
+        //y=z;
+        //System.out.println("apres permutation, x : " +x +"  y: " +y );
         
        
         //hello World
