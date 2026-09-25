3fc5debf632eb66142d3c1c3f5f0014ac5c718ed
ejehannin <ejehannin@350028R-F115X12.lise.kreizenn.bzh>
2026-09-25 15:06:26 +0200

    EXERCICE 5

--- src/pkg00/helloworld/HelloWorld.java
+++ src/pkg00/helloworld/HelloWorld.java
@@ -14,6 +14,7 @@ public class HelloWorld {
     /**
      * @param args the command line arguments
      */
+    //EXO 5
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
